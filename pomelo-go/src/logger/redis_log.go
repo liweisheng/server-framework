8764b3cbcd017a5c74520217873e88048f73dc8e
liweisheng <liwsustc@163.com>
2015-07-12 22:38:18 +0800

    Update redis_log.go

--- pomelo-go/src/logger/redis_log.go
+++ pomelo-go/src/logger/redis_log.go
@@ -1,3 +1,7 @@
+/**
+ * 向redis服务器写日志.
+ */
+
 package logger
 
 import (
@@ -15,10 +19,14 @@ const (
 	LOG_FATAL = iota
 )
 
+/// 在日志写向服务器之前，格式化日志.
+///
+/// 实现该接口可以自定义格式化器.
 type Formatter interface {
 	Format(msg []byte) []byte
 }
 
+/// 默认的日志格式化器
 type defaultFormatter struct{}
 
 func (*defaultFormatter) Format(msg []byte) []byte {
@@ -27,14 +35,21 @@ func (*defaultFormatter) Format(msg []byte) []byte {
 	return []byte(afterFmt)
 }
 
+
 type Logger struct {
-	logLev   uint8
-	servAddr string
-	servPort int
+	logLev   uint8   /// 限制日志输出级别，大于等于这个级别可以输出.
+	servAddr string	 /// redis服务器地址
+	servPort int     /// redis服务器端口
 	client   redis.Client
-	fmter    Formatter
+	fmter    Formatter /// 格式化器
 }
 
+/// 创建新的日志记录器.
+///
+/// @param servAddr redis服务器地址
+/// @param servPort redis服务器端口
+/// @param logLev 限制日志输出级别
+/// @return 成功则返回日志记录器，同时error为nil.
 func NewLogger(servAddr string, servPort int, logLev uint8) (*Logger, error) {
 	spec := redis.DefaultSpec().Host(servAddr).Port(servPort)
 	cli, err := redis.NewSynchClientWithSpec(spec)
@@ -46,10 +61,12 @@ func NewLogger(servAddr string, servPort int, logLev uint8) (*Logger, error) {
 	return &Logger{logLev, servAddr, servPort, cli, new(defaultFormatter)}, nil
 }
 
+/// 设置格式化器.
 func (log *Logger) SetFormatter(fmt Formatter) {
 	log.fmter = fmt
 }
 
+/// 写如FATAL级别日志.
 func (log *Logger) Fatal(msg []byte) {
 	if log.logLev > LOG_FATAL {
 		return
@@ -58,6 +75,7 @@ func (log *Logger) Fatal(msg []byte) {
 	log.client.Lpush("FATAL", msg)
 }
 
+/// ERROR级别日志.
 func (log *Logger) Error(msg []byte) {
 	if log.logLev > LOG_ERROR {
 		return
@@ -66,6 +84,7 @@ func (log *Logger) Error(msg []byte) {
 	log.client.Lpush("ERROR", msg)
 }
 
+/// WARN级别日志.
 func (log *Logger) Warn(msg []byte) {
 	if log.logLev > LOG_WARN {
 		return
@@ -74,6 +93,7 @@ func (log *Logger) Warn(msg []byte) {
 	log.client.Lpush("WARN", msg)
 }
 
+/// DEBUG级别日志.
 func (log *Logger) Debug(msg []byte) {
 	if log.logLev > LOG_DEBUG {
 		return
@@ -82,6 +102,7 @@ func (log *Logger) Debug(msg []byte) {
 	log.client.Lpush("DEBUG", msg)
 }
 
+/// INFO级别.
 func (log *Logger) Info(msg []byte) {
 	if log.logLev > LOG_INFO {
 		return
@@ -90,6 +111,7 @@ func (log *Logger) Info(msg []byte) {
 	log.client.Lpush("INFO", msg)
 }
 
+/// 写入由参数level指定的级别的日志.
 func (log *Logger) Log(level uint8, msg []byte) {
 	if log.logLev > level {
 		return
